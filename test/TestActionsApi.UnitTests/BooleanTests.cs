16754d066c6d1073f007f18ec729ffcdd75330b5
Neil Culver <neiljculver@gmail.com>
2020-08-23 15:59:52 +0100

    Add some tests

---
+++ test/TestActionsApi.UnitTests/BooleanTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace TestActionsApi.UnitTests
+{
+    public class BooleanTests
+    {
+        [Fact]
+        public void TrueIsTrue()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FalseIsFalse()
+        {
+            Assert.False(false);
+        }
+    }
+}
